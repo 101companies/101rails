a5aae5fcaf7d7a909312fb776bb805d7844970bb
Andrei Varanovich <dotnetby@gmail.com>
2013-02-25 12:47:25 +0100

    Fixed soruce code html layout

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -15,27 +15,8 @@ module PagesHelper
     return html.html_safe
   end 
   
-  def highlight(input)
-    puts substring_positions('<syntaxhighlight', input)
-    fragments = []
-    substring_positions('<syntaxhighlight', input).each do |idx|
-      e = input.index('</syntaxhighlight>', idx + 1)
-      fragment = input[idx, e + 18 - idx]
-      fragments.push(fragment)
-    end  
-
-    fragments.each do |fragment|
-      matches = fragment.match /<syntaxhighlight lang="(\w+)">([\S\s]+)<\/syntaxhighlight>/
-      lang = matches[1]
-      code = matches[2]
-      highlighted = Pygments.highlight(code, :lexer => lang)
-      highlighted.gsub!('<pre>', '!START!')
-      highlighted.gsub!('</pre>', '!END!')
-      input.gsub!(fragment, highlighted)
-    end  
-
-    input
-  end  
+  #      highlighted.gsub!('<pre>', '!START!')
+  #    highlighted.gsub!('</pre>', '!END!')
 
   def substring_positions(substring, string)
     string.enum_for(:scan, substring).map { $~.offset(0)[0] }
