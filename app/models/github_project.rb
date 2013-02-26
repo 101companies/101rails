4cff25890294744886bc8abd12b2313f48d7c5a3
Aleksey Lashin <aleksey.lashin@gmail.com>
2013-02-26 15:37:24 +0100

    admin interfaces, draft of submission
    simple administration with rails_admin
    new default font 'Open Sans'
    better footer
    draft for contribution submission
    replaced font-awesome with gem (rails_admin deps)

---
+++ app/models/github_project.rb
@@ -0,0 +1,7 @@
+class GithubProject
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  field :url, type: String
+  field :created_at, type: DateTime
+  field :updated_at, type: DateTime
+end
