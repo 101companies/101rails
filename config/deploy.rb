277c181976762f1aa27ac1112fa9676c9dcc3080
Andrei Varanovich <dotnetby@gmail.com>
2013-04-10 11:04:28 +0200

    Updated target deploy to 101companies.org

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,12 @@
-#require "rvm/capistrano"  
-require 'bundler/capistrano'
+ require 'bundler/capistrano'
+ load 'deploy/assets'
 
 logger.level = Logger::DEBUG
 
 set :application, "101wiki"
+set :repository, "git://github.com/101companies/101rails.git"  # Your clone URL
+set :branch, "master"
+
 set :rake, "#{rake} --trace"
 
 set :scm, :git
@@ -12,34 +15,25 @@ set :scm, :git
 
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "beta.101companies.org"                          # Your HTTP server, Apache/etc
-role :app, "beta.101companies.org"                          # This may be the same as your `Web` server
-role :db,  "beta.101companies.org", :primary => true # This is where Rails migrations will run
+role :web, "101companies.org"                          # Your HTTP server, Apache/etc
+role :app, "101companies.org"                          # This may be the same as your `Web` server
+role :db,  "101companies.org", :primary => true # This is where Rails migrations will run
 
 set :user, "user101"
-set :host, 'beta.101companies.org'
+set :host, '101companies.org'
 
 default_run_options[:pty] = true  # Must be set for the password prompt
                                   # from git to work
-set :repository, "git://github.com/101companies/101rails.git"  # Your clone URL
-set :branch, "master"
 set :use_sudo, true
 set :user, "user101"
 
 set :deploy_to, "/home/user101/wiki"
-set :deploy_via, :rsync_with_remote_cache #:remote_cache.
+set :deploy_via, :remote_cache
 ssh_options[:forward_agent] = true
 ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "id_rsa")] 
 
-#set :rvm_ruby_string, '1.9.3@101wiki'
-#set :rvm_type, :user
-
-#set :user, "deployer"  # The server's user for deploys
-#set :scm_passphrase, "p@ssw0rd"  # The deploy user's password
-
 # if you want to clean up old releases on each deploy uncomment this:
-after "deploy:restart", "deploy:cleanup"
-#before "deploy:update_code", "deploy:compress_assets"
+after "deploy:assets", "deploy:restart", "deploy:cleanup"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
@@ -53,13 +47,24 @@ after "deploy:restart", "deploy:cleanup"
   end
  end
 
-namespace :deploy do
-  namespace :assets do
-    desc 'Run the precompile task locally and rsync with shared'
-    task :precompile, :roles => :web, :except => { :no_release => true } do
-      %x{bundle exec rake assets:precompile}
-      %x{rsync --recursive --times --rsh=ssh --compress --human-readable --progress public/assets #{user}@#{host}:#{shared_path}}
-      %x{bundle exec rake assets:clean}
+namespace :assets do
+  task :precompile, :roles => :web, :except => { :no_release => true } do
+    # Check if assets have changed. If not, don't run the precompile task - it takes a long time.
+    force_compile = false
+    changed_asset_count = 0
+    begin
+      from = source.next_revision(current_revision)
+      asset_locations = 'app/assets/ lib/assets vendor/assets'
+      changed_asset_count = capture("cd #{latest_release} && #{source.local.log(from)} #{asset_locations} | wc -l").to_i
+    rescue Exception => e
+      logger.info "Error: #{e}, forcing precompile"
+      force_compile = false
+    end
+    if changed_asset_count > 0 || force_compile
+      logger.info "#{changed_asset_count} assets have changed. Pre-compiling"
+      run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+    else
+      logger.info "#{changed_asset_count} assets have changed. Skipping asset pre-compilation"
     end
   end
 end
