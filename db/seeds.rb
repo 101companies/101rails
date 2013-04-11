335699bdb2d1fc449e45424738d16449ab25d001
arkadi-schmidt <arkadi@uni-koblenz.de>
2013-04-11 22:31:52 +0200

    simple roles handling
    added script for creating supedadmin via 'bundle exec rake db:seed'
    added handling roles 'admin' and 'editor'

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,30 +7,11 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 puts 'SETTING UP DEFAULT USER LOGIN'
 
-def create_user(name, email, password, role)
+password = SecureRandom.urlsafe_base64
 
-  # find user by email
-  user = User.where(email: email).first
+User.create!  :name => "superadmin",
+              :email => "gatekeepers@101companies.org",
+              :password => password,
+              :role => "admin"
 
-  # if doesn't exist -> create him
-  if !user
-    user = User.create! :name => name,
-                        :email => email,
-                        :password => password,
-                        :password_confirmation => password,
-                        :role => role
-    puts 'New user created: ' << user.name
-  else
-    user.role = "admin"
-    user.save
-    puts 'Updated role to "admin" for ' << user.name
-  end
-
-end
-
-puts 'DON\'T DO IT IN PRODUCTION!!!'
-create_user('Andrei Varanovich', 'dotnetby@gmail.com', 'tester', 'admin')
-create_user('Olexiy Lashyn', 'aleksey.lashin@gmail.com', 'tester', 'admin')
-create_user('Arkadi Schmidt', 'arkadi.schmidt@gmail.com', 'tester', 'admin')
-create_user('Ralf Lämmel', 'rlaemmel@gmail.com', 'tester', 'admin')
-create_user('Thomas Schmorleiz', 'tschmorleiz@gmail.com', 'tester', 'admin')
+puts "created superadmin with password: " + password
