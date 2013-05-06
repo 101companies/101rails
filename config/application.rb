fb1a25cd71dfe28d7d2e1e5414b37fc8b00320d8
Aleksey Lashin <aleksey.lashin@gmail.com>
2013-05-06 14:35:38 +0200

    configured timezone for app

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Wiki
     config.generators do |g|
       g.view_specs false
       g.helper_specs false
-      
+
     end
 
     # Settings in config/environments/* take precedence over those specified here.
@@ -43,7 +43,7 @@ module Wiki
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Europe/Berlin'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
