afdacb35d3b68ff2e01cce23d4fd50ce3dc50acf
Aleksey Lashin <aleksey.lashin@gmail.com>
2013-06-11 21:41:42 +0200

    comments

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -90,23 +90,10 @@ class PagesController < ApplicationController
   end
 
   def get_rdf_graph(title, directions=false)
-     #   public static DEPENDS_ON = 'http://101companies.org/property/dependsOn'
-     #   public static IDENTIFIES = 'http://101companies.org/property/identifies'
-     #   public static LINKS_TO = 'http://101companies.org/property/linksTo'
-     #   public static CITES = 'http://101companies.org/property/cites'
-     #   public static USES = 'http://101companies.org/property/uses'
-     #   public static IMPLEMENTS = 'http://101companies.org/property/implements'
-     #   public static INSTANCE_OF = 'http://101companies.org/property/instanceOf'
-     #   public static IS_A = 'http://101companies.org/property/isA'
-     #   public static DEVELOPED_BY = 'http://101companies.org/property/developedBy'
-     #   public static REVIEWED_BY = 'http://101companies.org/property/reviewedBy'
-     #   public static RELATES_TO = 'http://101companies.org/property/relatesTo'
-     #   public static LABEL = 'http://www.w3.org/2000/01/rdf-schema#label'
-     #   public static PAGE = 'http://semantic-mediawiki.org/swivt/1.0#page'
-     #   public static TYPE = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'
      @page = Page.find_by_full_title(title)
 
      uri = self.page_to_resource title
+     #TODO: unused variable
      v101 = RDF::Vocabulary.new("http://101companies.org/property/")
      graph = RDF::Graph.new #<< [uri, RDF::RDFS.title, title]
 
