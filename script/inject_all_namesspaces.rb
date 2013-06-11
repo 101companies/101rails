fe46703622d4275bfe8265a1c070fcd53ff08934
Thomas Schmorleiz <tschmorleiz@googlemail.com>
2013-06-11 20:19:21 +0200

    adding script to inject namespace triples in all pages

--- script/inject_all_namesspaces.rb
+++ script/inject_all_namesspaces.rb
@@ -2,11 +2,12 @@ require '../config/environment.rb'
 require 'media_wiki'
 include ActionView::Helpers::PagesHelper
 
+# inject all namespace triples by triggering "change"
 all_pages.each do |title|
   p = Page.new.create(title)
   if p.content
     print title + ': Injecting...'
     p.change(p.content)
+    print " done.\n"
   end
-  print " done.\n"
 end
