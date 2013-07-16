2028ddf95ae5fe2ad4db4c42c7cb64539b0d49d3
Aleksey Lashin <aleksey.lashin@gmail.com>
2013-07-16 19:38:29 +0200

    new ui for admin interface

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -20,10 +20,11 @@ RailsAdmin.config do |config|
 
   ################  Global configuration  ################
 
+  # history of object changes with mongodb
+  #config.audit_with :mongoid_audit, 'HistoryTracker'
+
   # Set the admin name here (optional second array element will appear in red). For example:
-  config.main_app_name = ['Wiki', 'Admin']
-  # or for a more dynamic name:
-  # config.main_app_name = Proc.new { |controller| [Rails.application.engine_name.titleize, controller.params['action'].titleize] }
+  config.main_app_name = ['101Wiki', 'Admin']
 
   # RailsAdmin may need a way to know who the current user is]
   config.current_user_method { current_user } # auto-generated
@@ -31,149 +32,205 @@ RailsAdmin.config do |config|
   # use roles system for blocking admin interface
   config.authorize_with :cancan
 
-  # If you want to track changes on your models:
-  # config.audit_with :history, 'User'
-
-  # Or with a PaperTrail: (you need to install it first)
-  # config.audit_with :paper_trail, 'User'
-
   # Display empty fields in show views:
-  # config.compact_show_view = false
-
+  config.compact_show_view = false
   # Number of default rows per-page:
-  # config.default_items_per_page = 20
+  config.default_items_per_page = 30
 
   # Exclude specific models (keep the others):
-  # config.excluded_models = ['Authentication', 'User']
+  config.excluded_models = ['History', 'HistoryTracker']
 
-  # Include specific models (exclude the others):
-  # config.included_models = ['Authentication', 'User']
+  config.model 'Authentication' do
 
-  # Label methods for model instances:
-  # config.label_methods << :description # Default is [:name, :title]
+    field :user
 
+    field :uid do
+      searchable true
+    end
 
-  ################  Model configuration  ################
-
-  # Each model configuration can alternatively:
-  #   - stay here in a `config.model 'ModelName' do ... end` block
-  #   - go in the model definition file in a `rails_admin do ... end` block
-
-  # This is your choice to make:
-  #   - This initializer is loaded once at startup (modifications will show up when restarting the application) but all RailsAdmin configuration would stay in one place.
-  #   - Models are reloaded at each request in development mode (when modified), which may smooth your RailsAdmin development workflow.
-
-
-  # Now you probably need to tour the wiki a bit: https://github.com/sferik/rails_admin/wiki
-  # Anyway, here is how RailsAdmin saw your application's models when you ran the initializer:
-
-  config.model 'Contribution' do
-
-    configure :url
+    field :provider do
+      searchable true
+      pretty_value do
+        "<i class='icon-#{value}'> #{value}".html_safe
+      end
+    end
 
   end
 
-  ###  Authentication  ###
+  config.model 'Contribution' do
 
-  # config.model 'Authentication' do
+    field :title do
+      searchable true
+    end
 
-  #   # You can copy this to a 'rails_admin do ... end' block inside your authentication.rb model definition
+    field :url do
+      searchable true
+      label do
+        'Repo url'
+      end
+      pretty_value do
+        "<a target='_blank' href='#{value}'/>#{value}</a>".html_safe
+      end
+    end
 
-  #   # Found associations:
+    field :approved
+    field :analyzed
 
-  #     configure :user, :belongs_to_association
+    field :user do
+      searchable true
+    end
 
-  #   # Found columns:
+    field :page
 
-  #     configure :_type, :text         # Hidden
-  #     configure :_id, :bson_object_id
-  #     configure :created_at, :datetime
-  #     configure :updated_at, :datetime
-  #     configure :provider, :string
-  #     configure :uid, :string
-  #     configure :user_id, :bson_object_id         # Hidden
+    field :languages do
+      searchable true
+      pretty_value do
+        Contribution.array_to_string value
+      end
+    end
 
-  #   # Cross-section configuration:
+    field :concepts do
+      searchable true
+      pretty_value do
+        Contribution.array_to_string value
+      end
+    end
 
-  #     # object_label_method :name     # Name of the method called for pretty printing an *instance* of ModelName
-  #     # label 'My model'              # Name of ModelName (smartly defaults to ActiveRecord's I18n API)
-  #     # label_plural 'My models'      # Same, plural
-  #     # weight 0                      # Navigation priority. Bigger is higher.
-  #     # parent OtherModel             # Set parent model for navigation. MyModel will be nested below. OtherModel will be on first position of the dropdown
-  #     # navigation_label              # Sets dropdown entry's name in navigation. Only for parents!
+    field :technologies do
+      searchable true
+      pretty_value do
+        Contribution.array_to_string value
+      end
+    end
 
-  #   # Section specific configuration:
+    field :features do
+      searchable true
+      pretty_value do
+        Contribution.array_to_string value
+      end
+    end
 
-  #     list do
-  #       # filters [:id, :name]  # Array of field names which filters should be shown by default in the table header
-  #       # items_per_page 100    # Override default_items_per_page
-  #       # sort_by :id           # Sort column (default is primary key)
-  #       # sort_reverse true     # Sort direction (default is true for primary key, last created first)
-  #     end
-  #     show do; end
-  #     edit do; end
-  #     export do; end
-  #     # also see the create, update, modal and nested sections, which override edit in specific cases (resp. when creating, updating, modifying from another model in a popup modal or modifying from another model nested form)
-  #     # you can override a cross-section field configuration in any section with the same syntax `configure :field_name do ... end`
-  #     # using `field` instead of `configure` will exclude all other fields and force the ordering
-  # end
+    field :description do
+      searchable true
+      hide
+    end
 
+    field :created_at
+    field :updated_at
 
-  ###  User  ###
+  end
 
-  # config.model 'User' do
+  config.model 'OldWikiUser' do
 
-  #   # You can copy this to a 'rails_admin do ... end' block inside your user.rb model definition
+    field :email do
+      searchable true
+      pretty_value do
+        "<a href='mailto:#{value}'/>#{value}</a>".html_safe
+      end
+    end
 
-  #   # Found associations:
+    field :name do
+      searchable true
+    end
 
-  #     configure :authentications, :has_many_association
+    field :user do
+      searchable true
+    end
 
-  #   # Found columns:
+  end
 
-  #     configure :_type, :text         # Hidden
-  #     configure :_id, :bson_object_id
-  #     configure :created_at, :datetime
-  #     configure :updated_at, :datetime
-  #     configure :email, :text
-  #     configure :password, :password         # Hidden
-  #     configure :password_confirmation, :password         # Hidden
-  #     configure :reset_password_token, :text         # Hidden
-  #     configure :reset_password_sent_at, :datetime
-  #     configure :remember_created_at, :datetime
-  #     configure :sign_in_count, :integer
-  #     configure :current_sign_in_at, :datetime
-  #     configure :last_sign_in_at, :datetime
-  #     configure :current_sign_in_ip, :text
-  #     configure :last_sign_in_ip, :text
-  #     configure :confirmation_token, :text
-  #     configure :confirmed_at, :datetime
-  #     configure :confirmation_sent_at, :datetime
-  #     configure :name, :string
+  config.model 'User' do
 
-  #   # Cross-section configuration:
+    field :github_avatar do
+      label do
+        'Avatar'
+      end
+      pretty_value do
+        "<img width=24 height=24 src='#{value}'/>".html_safe
+      end
+    end
 
-  #     # object_label_method :name     # Name of the method called for pretty printing an *instance* of ModelName
-  #     # label 'My model'              # Name of ModelName (smartly defaults to ActiveRecord's I18n API)
-  #     # label_plural 'My models'      # Same, plural
-  #     # weight 0                      # Navigation priority. Bigger is higher.
-  #     # parent OtherModel             # Set parent model for navigation. MyModel will be nested below. OtherModel will be on first position of the dropdown
-  #     # navigation_label              # Sets dropdown entry's name in navigation. Only for parents!
+    field :name do
+      searchable true
+    end
 
-  #   # Section specific configuration:
+    field :email do
+      searchable true
+      pretty_value do
+        "<a href='mailto:#{value}'/>#{value}</a>".html_safe
+      end
+    end
 
-  #     list do
-  #       # filters [:id, :name]  # Array of field names which filters should be shown by default in the table header
-  #       # items_per_page 100    # Override default_items_per_page
-  #       # sort_by :id           # Sort column (default is primary key)
-  #       # sort_reverse true     # Sort direction (default is true for primary key, last created first)
-  #     end
-  #     show do; end
-  #     edit do; end
-  #     export do; end
-  #     # also see the create, update, modal and nested sections, which override edit in specific cases (resp. when creating, updating, modifying from another model in a popup modal or modifying from another model nested form)
-  #     # you can override a cross-section field configuration in any section with the same syntax `configure :field_name do ... end`
-  #     # using `field` instead of `configure` will exclude all other fields and force the ordering
-  # end
+    field :role, :enum do
+      searchable true
+      enum do
+        User.role_options
+      end
+    end
+
+    field :github_name do
+      searchable true
+      pretty_value do
+        "<a target='_blank' href='https://github.com/#{value}'/>#{value}</a>".html_safe
+      end
+    end
+
+    field :created_at
+    field :updated_at
+
+    field :pages do
+      searchable true
+    end
+
+    field :contributions do
+      searchable true
+    end
+
+    field :old_wiki_users do
+      searchable true
+    end
+
+    field :authentications do
+      searchable true
+    end
+
+  end
+
+  config.model 'Page' do
+
+    field :full_title do
+      label do
+        'Wiki Title'
+      end
+      searchable true
+    end
+
+    field :namespace do
+      hide
+      searchable true
+    end
+
+    field :title do
+      hide
+      searchable true
+    end
+
+    field :users
+    field :contribution
+
+    field :created_at
+    field :updated_at
+
+    edit do
+      field :namespace do
+        show
+      end
+
+      field :title do
+        show
+      end
+    end
+
+  end
 
 end
