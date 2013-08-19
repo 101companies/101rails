ed3852e0db21bf7cfb5a49bb435c8ba1289ce295
Aleksey Lashin <aleksey.lashin@gmail.com>
2013-08-19 23:22:45 +0200

    better work with oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Wiki::Application.config.middleware.use OmniAuth::Builder do
-  Rails.env == 'development' ? provider_suffix = '_DEV' : provider_suffix = ''
+  Rails.env == 'production' ? provider_suffix = '' : provider_suffix = '_DEV'
   provider :github, ENV['GITHUB_KEY' + provider_suffix], ENV['GITHUB_SECRET' + provider_suffix]
 end
