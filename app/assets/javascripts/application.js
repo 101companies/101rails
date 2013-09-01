1106b2f3c7032086433e8e39fc35c44945608252
Aleksey Lashin <aleksey.lashin@gmail.com>
2013-09-01 21:37:26 +0200

    formating in app.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,33 +1,35 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
-//= require jquery
-//= require typo/typo
-//= require spellcheck_ace/spellcheck_ace
-//= require jquery_ujs
-//= require jquery-ui
-//= require underscore
-//= require backbone
-//= require backbone_rails_sync
-//= require backbone_datalink
-//= require backbone/wiki
-//= require_tree .
-//= require bootstrap
-//= require ace/ace
-//= require ace/ext-searchbox
-//= require ace/theme-chrome
-//= require ace/theme-textmate
-//= require ace/theme-wiki
-//= require ace/mode-wiki
-//= require gritter
-//= require bootstrap-tour
-//= require moment.min
+/*
+ This is a manifest file that'll be compiled into application.js, which will include all the files
+ listed below.
+
+ Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+ or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+
+ It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+ the compiled file.
+
+ WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+ GO AFTER THE REQUIRES BELOW.
+
+ = require jquery
+ = require typo/typo
+ = require spellcheck_ace/spellcheck_ace
+ = require jquery_ujs
+ = require jquery-ui
+ = require underscore
+ = require backbone
+ = require backbone_rails_sync
+ = require backbone_datalink
+ = require backbone/wiki
+ = require_tree .
+ = require bootstrap
+ = require ace/ace
+ = require ace/ext-searchbox
+ = require ace/theme-chrome
+ = require ace/theme-textmate
+ = require ace/theme-wiki
+ = require ace/mode-wiki
+ = require gritter
+ = require bootstrap-tour
+ = require moment.min
+ */
