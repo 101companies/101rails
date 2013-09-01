991d52ff6963b2e0d3c6397b220d1d526e6d5e92
Aleksey Lashin <aleksey.lashin@gmail.com>
2013-09-01 21:37:12 +0200

    removed unused for 3 monthes script for passenger monitoring

--- script/passenger_monitor.rb
+++
@@ -1,8 +0,0 @@
-file_path = File.expand_path(File.dirname(__FILE__))
-# Load PassengerMonitor from '/lib/passenger_monitor.rb'
-require File.join(file_path, '..', 'lib', 'passenger_monitor')
- 
-# Set logger to log into Rails project /log directory and start monitoring
-PassengerMonitor.run(
-  :log_file => File.join(file_path, '..', 'log', 'passenger_monitor.log')
-)
