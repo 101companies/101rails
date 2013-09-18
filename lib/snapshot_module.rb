e377721b6abb3c1fa7acc8d583f51c3cdfcdbf8a
Andrei Varanovich <dotnetby@gmail.com>
2013-09-18 17:23:21 +0200

    Added caching for snapshots

--- lib/snapshot_module.rb
+++ lib/snapshot_module.rb
@@ -6,12 +6,12 @@ module SnapshotModule
 
   def self.get_snapshot(page)
     @b = Watir::Browser.new :phantomjs
-    @b.driver.manage.timeouts.implicit_wait = 10 #3 seconds
+    @b.driver.manage.timeouts.implicit_wait = 10 #10 seconds
     @site = "http://101companies.org/"
     @b.goto "#{@site}wiki/#{page.nice_wiki_url}"
     #@b.div(:class, 'triple').wait_until_present
-    @b.div(:id, 'backlinks').wait_until_present
-    @b.div(:class, 'content-loading').wait_while_present  
+    #@b.div(:id, 'backlinks').wait_until_present
+    #@b.div(:class, 'content-loading').wait_while_present  
     @doc = Nokogiri.HTML(@b.html) # Parse the document
     @doc.css('script').remove     # Remove <script>…</script>
     @b.close
