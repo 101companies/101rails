0555b10a284b2bd2edf6828a725b16a95e36d5cd
Thomas Schmorleiz <tschmorleiz@googlemail.com>
2013-09-23 16:53:19 -0400

    precompile css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Wiki::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( style.css )
+  config.assets.precompile += %w( clones.css style.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
