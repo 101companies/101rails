f4945f067f118c4e43f819034b6c152ad155c912
Thomas Schmorleiz <tschmorleiz@googlemail.com>
2013-09-24 01:27:59 -0400

    typo

--- app/models/clone.rb
+++ app/models/clone.rb
@@ -25,8 +25,8 @@ class Clone
   end
 
   def self.trigger_preparation
-    triggerurl = 'worker.101companies.org/services/triggerCloneCreation'
-    return open(url).read
+    triggerurl = 'http://worker.101companies.org/services/triggerCloneCreation'
+    return open(triggerurl).read
   end
 
 end
