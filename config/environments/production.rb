82cb82da0452bf9166b5b3242f95a7cd44bd4c2e
Aleksey Lashin <aleksey.lashin@gmail.com>
2014-05-03 14:19:30 +0200

    removed old css file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Wiki::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( clones.css style.css )
+  config.assets.precompile += %w( clones.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
