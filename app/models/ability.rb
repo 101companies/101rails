a4d72d85d75b95a0e3ded6fe86e1ebfd8af0bcc5
Aleksey Lashin <aleksey.lashin@gmail.com>
2014-06-10 17:07:18 +0200

    user can edit own contributor page

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,6 @@ class Ability
 
   def initialize(user)
 
-    # get exsiting user or create new temporary user
     user ||= User.new
 
     # admin can manage everyting and has access to admin ui
@@ -20,6 +19,9 @@ class Ability
 
     # user can be manually has permissions to change concrete page
     can :manage, Page, :user_ids => user.id
+
+    # user can create own contribution page
+    can :manage, Page, :title => user.github_name, :namespace => 'Contributor'
 
     cannot :history, :all
   end
