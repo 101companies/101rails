47201c40b4bdc46688b3b95ca09cfb02271e05e8
Aleksey Lashin <aleksey.lashin@gmail.com>
2014-06-15 23:10:32 +0200

    changes auto-generated text for contribution

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -77,6 +77,7 @@ class ContributionsController < ApplicationController
     @page.users << current_user
     @page.inject_namespace_triple
     @page.inject_triple "developedBy::Contributor:#{current_user.github_name}"
+    @page.inject_triple "carries::Tag:Stub"
     @page.save
 
     request = MatchingServiceRequest.new
