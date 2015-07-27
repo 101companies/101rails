4aea645366107fe8f1b87d2026a6a083200c2014
Kevin <kevin.k1252@gmail.com>
2015-07-27 13:29:56 +0200

    final changes

--- app/assets/javascripts/components/triple_editor.js.jsx
+++ app/assets/javascripts/components/triple_editor.js.jsx
@@ -1,28 +1,5 @@
 var TripleEditor = React.createClass({
 
-  render: function() {
-    if(!this.props.triple.predicate || !this.props.predicates[this.props.triple.predicate]) {
-      return <div></div>;
-    }
-
-    var predicateOptions = $.map(this.props.predicates, function(index, key) {
-      return <option value={key} key={key}>{key}</option>;
-    });
-
-    var objectOptions = $.map(this.props.predicates[this.props.triple.predicate], function(object) {
-      return <option value={object} key={object}>{object}</option>
-    });
-
-    return <div>
-      <select onChange={this.onPredicateChange} ref='predicateInput' value={this.props.triple.predicate}>
-        {predicateOptions}
-      </select>
-      <select onChange={this.onObjectChange} ref='objectInput' value={this.props.triple.object}>
-        {objectOptions}
-      </select>
-    </div>;
-  },
-
   componentDidMount: function() {
     $(React.findDOMNode(this.refs.objectInput)).autocomplete({
       source: this.props.pages,
@@ -30,21 +7,56 @@ var TripleEditor = React.createClass({
     });
   },
 
-  onObjectChange: function() {
+  onObjectChange: function(object) {
     this.props.onChange({
       id: this.props.triple.id,
       predicate: this.props.triple.predicate,
-      object: React.findDOMNode(this.refs.objectInput).value
+      object: object
     });
   },
 
-  onPredicateChange: function() {
-    var predicate = React.findDOMNode(this.refs.predicateInput).value;
+  onPredicateChange: function(predicate) {
     this.props.onChange({
       id: this.props.triple.id,
       predicate: predicate,
       object: this.props.predicates[predicate][0]
     });
+  },
+
+  render: function() {
+    if(!this.props.triple.predicate || !this.props.predicates[this.props.triple.predicate]) {
+      return <div></div>;
+    }
+
+    var predicateOptions = $.map(this.props.predicates, function(index, key) {
+      return {
+        value: key,
+        label: key
+      };
+    });
+
+    var objectOptions = $.map(this.props.predicates[this.props.triple.predicate], function(object) {
+      return {
+        value: object,
+        label: object
+      };
+    });
+
+    return <div className='row' style={{ marginLeft: 0 }}>
+      <div className='span6'>
+        <Select onChange={this.onPredicateChange}
+                options={predicateOptions}
+                ref='predicateInput'
+                value={this.props.triple.predicate} />
+      </div>
+
+      <div className='span6'>
+        <Select onChange={this.onObjectChange}
+                ref='objectInput'
+                options={objectOptions}
+                value={this.props.triple.object} />
+      </div>
+    </div>;
   }
 
 });
