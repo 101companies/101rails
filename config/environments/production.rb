ebb55b17c6451e9309e0a4cd14ca0c5ad44e584d
Kevin <kevin.k1252@gmail.com>
2015-11-29 22:00:50 +0100

    raise errors on email delivery fail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,7 @@ Wiki::Application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'no-reply@101companies.org'}
+  config.action_mailer.raise_delivery_errors = true
 
   config.eager_load = true
 
