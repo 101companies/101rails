7d03cb07ea3c8c239be14a2c3d7e0135fd085d81
Andreas Schmidt <andischmidt@uni-koblenz.de>
2016-12-02 18:43:31 +0100

    changed landing page for resources

--- app/controllers/resource_controller.rb
+++ app/controllers/resource_controller.rb
@@ -42,16 +42,22 @@ class ResourceController < ApplicationController
       port = ':'+request.port.to_s
       #port = ''
 
+      # + prepare subject ----------------------
+      @subject = RDF::URI.new(scheme: request.scheme.dup,
+                              authority: host + port,
+                              host: host,
+                              port: request.port,
+                              path: 'resource/101companies')
+      # - prepare subject ----------------------
+
       # + execute rdf querys -----------------
       respond_to do |format|
         format.json { render :json => [] }
         format.xml  { render :xml  => [].to_rdfxml }
         #format.rdf  { render :xml  => graph.query([@subject, :p, :o]).to_rdfxml }
         format.html {
-
           # + queries on graph -------------------
-          #.select{ |s,p,o| o.to_s.downcase.include?('namespace') }
-          @result = graph.query([:s, RDF::type, :o]).to_a.uniq{|s,p,o| o.pname}.sort_by { |s,p,o| o.pname }
+          @result = graph.query([:s, :p, @subject]).to_a.uniq{|s,p,o| s.pname}.sort_by { |s,p,o| s.pname }
           render file: 'resource/landing.html.erb'
         }
       end
@@ -120,7 +126,7 @@ class ResourceController < ApplicationController
             # html format without results tries a search
             search_str = params[:resource_name].downcase
 
-            if search_str.length > 4
+            if search_str.length > 2
               @result = graph.to_a.uniq{|s,p,o| s.pname }.select{ |s,p,o| s.pname.split('/').last.downcase.include?(search_str) }.sort_by { |s,p,o| s.pname }
             else
               @result = nil
@@ -135,18 +141,24 @@ class ResourceController < ApplicationController
 
   # helper function for render resources
   def render_resource (res)
-    if res.uri? # is an uri
-      if res.value.split(request.host).count < 2 # is an external link
-        view_context.link_to res.value.split('/').last, res.value, :target => "_blank"
-      else
-        view_context.link_to  res.value.split('/').last, res.value
-      end
-    else
+    if res.literal?
+      # literal
       require 'uri'
-      if (res.value =~ /\A#{URI::regexp}\z/)
+      if res.value =~ URI::regexp
+        # url literal
         view_context.link_to res.value, res.value, :target => "_blank"
       else
-        res.value
+        # text literal
+        view_context.raw res.value
+      end
+    else
+      # node
+      if res.value.split(request.host).count < 2
+        # external uri
+        view_context.link_to res.value.split('/').last, res.value, :target => "_blank"
+      else
+        # internal uri
+        view_context.link_to  res.value.split('/').last, res.value
       end
     end
   end
