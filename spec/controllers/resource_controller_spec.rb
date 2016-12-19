cf8e76e63709c29d548b57fc0749c4f7030450d3
Andreas Schmidt <andischmidt@uni-koblenz.de>
2016-12-19 21:58:16 +0100

    this was an autogenerated file without functionality

--- spec/controllers/resource_controller_spec.rb
+++ spec/controllers/resource_controller_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe ResourceController, type: :controller do
 
   describe "GET #get" do
     it "returns http success" do
-      get :get
+      get :get, params: { id: page.url, format: :json }
+
       expect(response).to have_http_status(:success)
     end
   end
