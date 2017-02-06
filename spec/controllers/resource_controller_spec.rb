181fcbe9ae07b13f98433bc23d797f98d4e560b5
Andreas Schmidt <andischmidt@uni-koblenz.de>
2017-02-06 18:35:34 +0100

    specifications for ResourceController (immature)

--- spec/controllers/resource_controller_spec.rb
+++ spec/controllers/resource_controller_spec.rb
@@ -1,50 +1,29 @@
 require 'rails_helper'
 
-=begin
-def check_graph
-  onto_path = Rails.root.join('../101web/data/dumps/ontology.ttl')
-
-  if (!File.exists?(onto_path))
-    @result = nil
-    render file: 'resource/search.html.erb'
-    false
-  else
-    true
-  end
-end
-=end
-
 RSpec.describe ResourceController, type: :controller do
 
-  describe 'GET resource' do
+  describe 'Ontology Graph' do
+    it 'should exist and not empty' do
+      expect($graph).not_to be_nil
 
-    it 'checks json result of 101companies' do
-
-      get :get, params: { resource_name: '101companies', host: 'localhost', format: :json }
-
-      json_reponse = JSON.parse(response.body)
-
-      expect(json_reponse).not_to eq({})
+      expect($graph.count).to be > 0
     end
 
-    it 'checks json result of not existing item' do
+    describe 'GET resource' do
 
-      get :get, params: { resource_name: '555b72c2-9d0d-47ed-8715-ececc2e117d9', host: 'localhost', format: :json }
+      xit 'returns resource' do
 
-      json_reponse = JSON.parse(response.body)
+        # get should return a simple json response of an existing resource. It should do it but it does not.
+        get(:get, params: { resource_name: '101companies' })
 
-      expect(json_reponse).to eq({})
+        # it expects a knows attribute of a simple json response of an existing resource
+        expect(true).to eq(response.body)
+
+      end
+
     end
-
-=begin
-    it 'returns the html page' do
-      get(:get, params: { resource_name: '101companies' })
-
-      expect(response).to render(:partial => 'resource/101companies')
-    end
-=end
-
   end
+end
 
 =begin
   describe 'GET get' do
@@ -91,4 +70,3 @@ RSpec.describe ResourceController, type: :controller do
   end
 =end
 
-end
