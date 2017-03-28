3ea4ae194a8670319b5ae3ce0c150ddd7c1cebed
kevin <kevin@tabolo.de>
2017-03-28 15:35:13 +0200

    got technologies back

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,7 +1,7 @@
 class LandingController < ApplicationController
 
   def index
-    # @technologies = Page.popular_technologies
+    @technologies = Page.popular_technologies
     # @popular_technology_pages = Page.popular_technology_pages
     # @recent_technology_pages = Page.technologies.recently_updated
   end
