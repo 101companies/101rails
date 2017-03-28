b62ea8996dd9cb828e3c939212cc57f7b264f71d
kevin <kevin@tabolo.de>
2017-03-28 15:33:33 +0200

    removed not now-needed stuff

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,9 +1,9 @@
 class LandingController < ApplicationController
 
   def index
-    @technologies = Page.popular_technologies
-    @popular_technology_pages = Page.popular_technology_pages
-    @recent_technology_pages = Page.technologies.recently_updated
+    # @technologies = Page.popular_technologies
+    # @popular_technology_pages = Page.popular_technology_pages
+    # @recent_technology_pages = Page.technologies.recently_updated
   end
 
 end
