05e6458cffa91b13e99a341ade1dce5c09824a05
kevin <kevin.k1252@gmail.com>
2017-05-02 18:15:23 +0200

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Wiki::Application.routes.draw do
 
   constraints(host: /101companies.org/) do
-    match "/(*path)" => redirect {|params, req| "https://101wiki.softlang.org/#{params[:path]&.gsub(' ', '_')}"},  via: [:get, :post]
+    match "/(*path)" => redirect { |params, req|
+      URI.encode("https://101wiki.softlang.org/#{params[:path]}")
+    },  via: [:get, :post]
   end
 
   resources :mappings, only: [:edit, :update]
@@ -82,7 +84,9 @@ Wiki::Application.routes.draw do
   end
 
   scope '/wiki' do
-    match "(*path)" => redirect {|params, req| "/#{params[:path].gsub(' ', '_')}"},  via: [:get, :post]
+    match "(*path)" => redirect { |params, req|
+      URI.encode(params[:path])
+    }, via: [:get, :post]
   end
 
 end
