8ba4c85d763cc11df69fad5be0edb1505203deb6
Kevin Klein <kevin@tabolo.de>
2017-06-26 22:00:45 +0200

    fixed #208

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -1,7 +1,19 @@
 /*
  = require jquery
- = require bootstrap
  = require d3
+ = require bootstrap
  = require d3.layout.cloud
  = require landing_tagcloud
 */
+
+$(document).ready(function() {
+  console.log('init');
+  var hash = window.location.hash;
+  hash && $('a[href="' + hash + '"]').tab('show');
+
+  $('.nav a').click(function (e) {
+    var scrollmem = $('body').scrollTop();
+    window.location.hash = this.hash;
+    $('html,body').scrollTop(scrollmem);
+  });
+});
