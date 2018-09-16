f9716bb6994e05833f55d3910269ee2dae34d405
Kevin Klein <kevin@tabolo.de>
2018-09-16 13:52:10 +0200

    ahoy fix

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -1,3 +1,12 @@
-class Ahoy::Store < Ahoy::Stores::ActiveRecordTokenStore
-  # customize here
+class Ahoy::Store < Ahoy::DatabaseStore
+  def authenticate(data)
+  end
 end
+
+Ahoy.exclude_method = lambda do |controller, request|
+  controller.current_user.nil?
+end
+
+Ahoy.mask_ips = true
+Ahoy.cookies = false
+Ahoy.geocode = false
