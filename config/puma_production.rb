ba8eb3f34bf53b8799d58b843976bf78d09843c8
kevin <kevin.k1252@gmail.com>
2021-04-20 15:36:36 +0200

    puma fix

--- config/puma_production.rb
+++ config/puma_production.rb
@@ -2,7 +2,8 @@
 
 rackup      DefaultRackup
 port        ENV['PORT'] || 3000
-environment ENV['RAILS_ENV'] || 'development'
+environment ENV['RAILS_ENV'] || 'production'
+
 
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
