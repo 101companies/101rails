4aa2058726060835afbadf46855b82d4cf7cb2da
kevin <kevin.k1252@gmail.com>
2021-04-20 15:33:12 +0200

    changed to single worker

--- config/puma_production.rb
+++ config/puma_production.rb
@@ -3,7 +3,6 @@
 rackup      DefaultRackup
 port        ENV['PORT'] || 3000
 environment ENV['RAILS_ENV'] || 'development'
-workers     Integer(ENV['WEB_CONCURRENCY'] || 1)
 
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
