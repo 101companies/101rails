c4bea405d9b019409f395be8a317e70581769133
kevin <kevin.k1252@gmail.com>
2021-04-21 10:54:52 +0200

    omniauth fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,3 +3,5 @@ Wiki::Application.config.middleware.use OmniAuth::Builder do
   # provider :github, ENV['GITHUB_KEY' + provider_suffix], ENV['GITHUB_SECRET' + provider_suffix]
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
 end
+
+OmniAuth.config.allowed_request_methods = [:post, :get]
