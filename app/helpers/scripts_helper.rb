8fc59542caa7adb2b2e5b570576cb02507c8b0a6
kevin <kevin.k1252@gmail.com>
2021-05-21 14:03:40 +0200

    fixed pdf links

--- app/helpers/scripts_helper.rb
+++ app/helpers/scripts_helper.rb
@@ -1,2 +1,12 @@
 module ScriptsHelper
+  def fix_pdf_links(text)
+    html = Nokogiri::HTML::DocumentFragment.parse(text)
+    html.search('a').each do |link|
+      unless link['href'].match(/^https?:\/\/.*$/)
+        link['href'] = 'https://101wiki.softlang.org' + link['href']
+      end
+    end
+
+    raw html.to_html
+  end
 end
